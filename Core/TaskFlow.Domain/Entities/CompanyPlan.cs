59f11b0d5291b84dca9ea626d3c6b8a82d941881
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-02 21:16:41 +0300

    MODEL DÜZENLENMESİ VE MİGRATİON ALINMASI YAPILDI

--- Core/TaskFlow.Domain/Entities/CompanyPlan.cs
+++ Core/TaskFlow.Domain/Entities/CompanyPlan.cs
@@ -5,8 +5,7 @@ namespace TaskFlow.Domain.Entities
     public class CompanyPlan : BaseEntity
     {
         public string PlanName { get; set; }
-        public Guid PlanPropertiesId { get; set; }
-        public PlanProperties PlanProperties { get; set; }
+        public ICollection<PlanProperties> PlanProperties { get; set; }
 
     }
 }
---
+++ Infrastructure/TaskFlow.Persistence/ApplicationContext/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TaskFlow.Persistence.ApplicationContext
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = "Host=localhost;Port=5432;Database=TaskFlowDb;Username=postgres;Password=emreraftongame63";
+            optionsBuilder.UseNpgsql(
+                connectionString
+            );
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}

52a8dc851a3ec60c6281d0c05953a9c543b70400
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-04 01:12:59 +0300

    REPOİTORY KATMANI EKLENDİ

---
+++ Core/TaskFlow.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using TaskFlow.Domain.Bases;
+
+namespace TaskFlow.Application.Repositories
+{
+    public interface IWriteRepository <T> : IBaseEntity
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+    }
+}

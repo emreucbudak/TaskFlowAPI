7086c15ceea983a02418eec812989c7ae9630bd6
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-04 08:47:56 +0300

    UNİTOFWORK EKLENDİ

---
+++ Infrastructure/TaskFlow.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFlow.Application.Repositories;
+using TaskFlow.Application.UnitOfWork;
+using TaskFlow.Persistence.ApplicationContext;
+using TaskFlow.Persistence.Repositories;
+
+namespace TaskFlow.Persistence.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public IReadRepository<T> GetReadRepository<T>() where T : class, new() => new ReadRepository<T>(_context);
+
+        public IWriteRepository<T> GetWriteRepository<T>() where T : class, new() => new WriteRepository<T>(_context);
+
+        public int SaveChanges()
+        {
+           return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}

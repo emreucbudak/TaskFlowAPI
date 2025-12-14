a7628c12f4e94b9fc963f7f3c15d04704e3b4c36
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-14 20:21:18 +0300

    PROJENİN MODÜLER MONOLİT MİAMRİYE GEÇİŞİ DEVAM EDİYOR

---
+++ src/BuildingBlocks/TaskFlow.BuildingBlocks/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.BuildingBlocks.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.CreateVersion7();
+    }
+}
---
+++ src/Modules/ProjectManagement/ProjectManagement.Domain/Entities/SubTaskAnswer.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectManagement.Domain.Entities
+{
+    public class SubTaskAnswer
+    {
+        public string AnswerText { get; set; }
+        public Guid SubTaskId { get; set; }
+        public ProjectManagement.Domain.Entities.Subtask SubTask { get; set; }
+    }
+}

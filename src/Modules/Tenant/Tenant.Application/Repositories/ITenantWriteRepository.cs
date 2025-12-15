7ae94e807048e498f716e7e16dddd870664c8523
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-15 23:30:24 +0300

    TENANTIN REPOSİTORY İNTERFACELERİ YAZILDI

---
+++ src/Modules/Tenant/Tenant.Application/Repositories/ITenantWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Tenant.Domain.Entities;
+
+namespace Tenant.Application.Repositories
+{
+    public interface ITenantWriteRepository
+    {
+        Task AddPlan(CompanyPlan plan);
+        Task DeletePlan (CompanyPlan plan);
+        Task UpdatePlan (CompanyPlan plan);
+        Task<CompanyPlan> GetPlan (Guid id);
+    }
+}

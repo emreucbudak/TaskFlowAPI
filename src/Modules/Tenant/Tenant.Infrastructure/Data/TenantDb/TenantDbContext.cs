f9ad1569997c97f91ba146ae5e465ea29f2c5d14
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-15 20:48:12 +0300

    TENANT MODULU EKLENDİ

---
+++ src/Modules/Tenant/Tenant.Infrastructure/Data/TenantDb/TenantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tenant.Infrastructure.Data.TenantDb
+{
+    public class TenantDbContext : DbContext
+    {
+        public TenantDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected TenantDbContext()
+        {
+        }
+        public DbSet<Tenant.Domain.Entities.Company> companies { get; set; }
+        public DbSet<Tenant.Domain.Entities.CompanyPlan> companyPlans { get; set; }
+        public DbSet<Tenant.Domain.Entities.PlanProperties> planProperties { get; set; }
+    }
+}

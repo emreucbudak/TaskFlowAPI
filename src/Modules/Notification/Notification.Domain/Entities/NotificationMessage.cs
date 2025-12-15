914798563b9e99837d36eaadf822b6f3e8c32bf5
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-15 23:48:39 +0300

    MODULER MONOLİTH MİMARİYE GEÇİŞ DEVAM EDİYOR

---
+++ src/Modules/Notification/Notification.Domain/Entities/NotificationMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Notification.Domain.Entities
+{
+    public class NotificationMessage : TaskFlow.BuildingBlocks.Common.BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime SendTime { get; set; }
+    }
+}
---
+++ src/Modules/Notification/Notification.Infrastructure/Data/NotificationDb/NotificationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notification.Domain.Entities;
+
+namespace Notification.Infrastructure.Data.NotificationDb
+{
+    public class NotificationDbContext : DbContext
+    {
+        public NotificationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected NotificationDbContext()
+        {
+        }
+        public DbSet<NotificationMessage> notificationMessages { get; set; }
+    }
+}

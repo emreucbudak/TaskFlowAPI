3ff44a79315797d97a48059ddfbee48242e19285
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-15 23:51:10 +0300

    MODULER MONOLİTH MİMARİDE CONFİGURASYONLAR EKLENDİ

---
+++ src/Modules/ProjectManagement/ProjectManagement.Infrastructure/Data/DataConfiguration/TaskPriorityCategorySeedConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectManagement.Domain.Entities;
+
+namespace ProjectManagement.Infrastructure.Data.DataConfiguration
+{
+    public class TaskPriorityCategorySeedConfiguration : IEntityTypeConfiguration<TaskPriorityCategory>
+    {
+        public void Configure(EntityTypeBuilder<TaskPriorityCategory> builder)
+        {
+            builder.HasData(new TaskPriorityCategory()
+            {
+                TaskPriorityCategoryId = 1,
+                CategoryName = "Öncelikli Görev"
+            },
+            new TaskPriorityCategory()
+            {
+                TaskPriorityCategoryId = 2,
+                CategoryName = "Sıradan Görev"
+            },
+            new TaskPriorityCategory()
+            {
+                TaskPriorityCategoryId = 3,
+                CategoryName = "Acil Görev"
+            });
+        }
+    }
+}
---
+++ src/Modules/ProjectManagement/ProjectManagement.Infrastructure/Data/DataConfiguration/TaskStatusSeedConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ProjectManagement.Infrastructure.Data.DataConfiguration
+{
+    public class TaskStatusSeedConfiguration : IEntityTypeConfiguration<Domain.Entities.TaskStatus>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.TaskStatus> builder)
+        {
+            builder.HasData(new Domain.Entities.TaskStatus()
+            {
+                TaskStatusId = 1,
+                StatusName = "Görev Ataması Yapıldı",
+            },
+            new Domain.Entities.TaskStatus()
+            {
+                TaskStatusId = 2,
+                StatusName = "Tamamlandı"
+            },
+            new Domain.Entities.TaskStatus()
+            {
+                TaskStatusId = 3,
+                StatusName = "Onay Bekliyor"
+            });
+        }
+    }
+}

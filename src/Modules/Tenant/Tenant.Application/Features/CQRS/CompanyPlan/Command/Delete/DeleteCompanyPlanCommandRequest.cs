422759b852899038234492ac1d44af42bbc1f066
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-17 21:00:54 +0300

    DELETECOMPANYPLANCOMMAND EKLENDİ

---
+++ src/Modules/Tenant/Tenant.Application/Features/CQRS/CompanyPlan/Command/Delete/DeleteCompanyPlanCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using FlashMediator.src.FlashMediator.Contracts;
+
+namespace Tenant.Application.Features.CQRS.CompanyPlan.Command.Delete
+{
+    public class DeleteCompanyPlanCommandRequest : IRequest
+    {
+        public Guid CompanyPlanId { get; init; }
+    }
+}

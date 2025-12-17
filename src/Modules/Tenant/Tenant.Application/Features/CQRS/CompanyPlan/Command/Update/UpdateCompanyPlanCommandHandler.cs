bccbabc3982edebf223ed3ebf1121a15ddec4c63
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-17 20:56:18 +0300

    TENANTIN COMPANY PLAN UPDATESİ EKLENDİ

---
+++ src/Modules/Tenant/Tenant.Application/Features/CQRS/CompanyPlan/Command/Update/UpdateCompanyPlanCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using FlashMediator.src.FlashMediator.Contracts;
+using Tenant.Application.Repositories;
+using Tenant.Application.UnitOfWork;
+
+namespace Tenant.Application.Features.CQRS.CompanyPlan.Command.Update
+{
+    internal class UpdateCompanyPlanCommandHandler : IRequestHandler<UpdateCompanyPlanCommandRequest>
+    {
+        private readonly ITenantWriteRepository tenantWriteRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UpdateCompanyPlanCommandHandler(ITenantWriteRepository tenantWriteRepository, IUnitOfWork unitOfWork)
+        {
+            this.tenantWriteRepository = tenantWriteRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(UpdateCompanyPlanCommandRequest request, CancellationToken cancellationToken)
+        {
+            var companyPlan = await tenantWriteRepository.GetPlan(request.CompanyPlanId, true);
+            var newProperties = new Domain.Entities.PlanProperties(
+                request.PeopleAddedLimit,
+                request.IsIncludeGroupChat,
+                request.TeamLimit,
+                request.IsDailyPlannerEnabled,
+                request.IsIncludeTaskPriorityCategory,
+                request.IsDeadlineNotificationEnabled,
+                request.IsIncludeAddTaskNotifications
+            );
+            companyPlan.UpdateProperties(newProperties);
+            await unitOfWork.SaveChangesAsync();
+        }
+    }
+}

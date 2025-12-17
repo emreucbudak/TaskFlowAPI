c39d8ca5752ef725903232216594c645ac37edf9
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-12-18 00:26:39 +0300

    MİMARİ DÖNÜŞÜM BÜYÜK ORANDA TAMAMLANDI

--- src/Modules/ProjectManagement/ProjectManagement.Domain/Entities/Task.cs
+++ src/Modules/ProjectManagement/ProjectManagement.Domain/Entities/Task.cs
@@ -4,13 +4,59 @@ namespace ProjectManagement.Domain.Entities
 {
     public class Task : BaseEntity
     {
-        public string TaskName { get; set; }
-        public string Description { get; set; }
-        public int TaskStatusId { get; set; }
-        public TaskStatus TaskStatus { get; set; }
-        public ICollection<TaskAnswer> TaskAnswer { get; set; } = new List<TaskAnswer>();
-        public DateTime DeadlineTime { get; set; }
-        public int? TaskPriorityCategoryId { get; set; }
-        public TaskPriorityCategory? TaskPriority { get; set; }
+        public Task(string taskName, string description, DateTime deadlineTime)
+        {
+            if (string.IsNullOrWhiteSpace(taskName))
+            {
+                throw new ArgumentException("Task Adı boş olamaz");
+            }
+            if (deadlineTime< DateTime.Now)
+            {
+                throw new ArgumentException("Bitiş tarihi şuandan önce (geçmiş tarih olamaz)");
+            }
+            TaskName = taskName;
+            Description = description;
+            TaskStatusId = 1;
+            DeadlineTime = deadlineTime;
+        }
+
+        public string TaskName { get; private set; }
+        public string Description { get; private set; }
+        public int TaskStatusId { get; private set; }
+        public TaskStatus TaskStatus { get; private set; }
+        private readonly List<TaskAnswer> _answers = new();
+        public IReadOnlyCollection<TaskAnswer> taskAnswers => _answers; 
+        private readonly List<Subtask> _subtask = new();
+        public IReadOnlyCollection<Subtask> subtask => _subtask;
+        public DateTime DeadlineTime { get; private set; }
+        public int? TaskPriorityCategoryId { get; private set; }
+        public TaskPriorityCategory? TaskPriority { get; private set; }
+        public void AddAnswer(string AnswerText)
+        {
+            var answer = new TaskAnswer()
+            {
+                AnswerText = AnswerText
+            };
+            _answers.Add(answer);
+        }
+        public void UpdateTaskPriority (int taskPriority)
+        {
+            this.TaskPriorityCategoryId = taskPriority;
+        }
+        public void UpdateTaskStatus(int taskStatusId)
+        {
+            this.TaskStatusId = taskStatusId;
+        }
+        public void AddSubTask (string description)
+        {
+            if (TaskStatusId == 2)
+            {
+                throw new InvalidOperationException("TAMAMLANMIŞ GÖREVE SUBTASK EKLENEMEZ");
+            }
+            var subtask = new Subtask(description);
+            _subtask.Add(subtask);
+        }
+
+        
     }
 }
--- src/Modules/ProjectManagement/ProjectManagement.Domain/Entities/TaskAnswer.cs
+++ src/Modules/ProjectManagement/ProjectManagement.Domain/Entities/TaskAnswer.cs
@@ -5,8 +5,7 @@ namespace ProjectManagement.Domain.Entities
     public class TaskAnswer : BaseEntity
     {
         public string AnswerText { get; set; }
-        public Guid TaskId { get; set; }
-        public Task Task { get; set; }
+
 
     }
 }

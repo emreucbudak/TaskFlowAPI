0e66800c5d7eec01686bea0b1b233ad7a9259763
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-01-22 02:04:39 +0300

    Bildirim servisi için mimari iyileştiriliyor

--- src/Modules/Identity/Identity.Infrastructure/Data/IdentityDb/IdentityManagementDbContext.cs
+++ src/Modules/Identity/Identity.Infrastructure/Data/IdentityDb/IdentityManagementDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Identity.Application.IDbContext;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Infrastructure.Data.IdentityDb
 {
-    public class IdentityManagementDbContext : IdentityDbContext<Domain.Entities.User, Domain.Entities.Roles, Guid>
+    public class IdentityManagementDbContext : IdentityDbContext<Domain.Entities.User, Domain.Entities.Roles, Guid>, IIdentityManagementDbContext
     {
         public IdentityManagementDbContext(DbContextOptions options) : base(options)
         {
@@ -14,5 +15,10 @@ namespace Identity.Infrastructure.Data.IdentityDb
         }
         public DbSet<Domain.Entities.User> Users { get; set; }
         public DbSet<Domain.Entities.Roles> Roles { get; set; }
+        public DbSet<Domain.Entities.Company> Companies { get; set; }
+        public DbSet<Domain.Entities.Groups> Groups { get; set; }
+        public DbSet<Domain.Entities.GroupsMember> GroupsMembers { get; set; }
+        public DbSet<Domain.Entities.GroupRoles> GroupRoles { get; set; }
+
     }
 }

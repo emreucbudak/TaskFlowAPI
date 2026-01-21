04b771c44d73e82ee9f57ab4e62ae1dbf088e534
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-01-22 01:00:38 +0300

    BİLDİRİM SİSTEMİNİN EKLENMESİNE DEVAM EDİLİYOR

--- src/Modules/Identity/Identity.Domain/Entities/Groups.cs
+++ src/Modules/Identity/Identity.Domain/Entities/Groups.cs
@@ -9,10 +9,18 @@
         {
             Name = name;
         }
-        public void AddUser(Guid userid)
+        public void AddUser(Guid userid,int rolesId)
         {
-            var groupMember = new GroupsMember(userid, this.Id);
+            var groupMember = new GroupsMember(userid, this.Id,rolesId);
             Users.Add(groupMember);
         }
+        public void RemoveUser(Guid userid) {
+            var userToRemove = Users.FirstOrDefault(u => u.UserId == userid);
+            if (userToRemove is not null)
+            {
+                Users.Remove(userToRemove);
+            }
+        }
+
     }
 }

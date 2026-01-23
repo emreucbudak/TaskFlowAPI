7a18a0fbba42b58a8ba91c07c95ab352dd55fc65
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-01-24 01:17:52 +0300

    bildirim servisi için iyileştirmeler devam ediliyor

--- src/Modules/Notification/Notification.Infrastructure/NotificationService/UserNotificationService.cs
+++ src/Modules/Notification/Notification.Infrastructure/NotificationService/UserNotificationService.cs
@@ -1,13 +1,25 @@
-﻿using Notification.Application;
+﻿using Microsoft.AspNetCore.SignalR;
+using Notification.Application;
+using Notification.Application.Repositories;
 using Notification.Domain.Models;
+using Notification.Infrastructure.Hubs;
 
 namespace Notification.Infrastructure.NotificationService
 {
-    public class UserNotificationService : INotificationService
+    public class UserNotificationService(IHubContext<NotificationHub> hubContext,INotificationWriteRepository rp) : INotificationService
     {
-        public Task SendNotificationToUserAsync(string userId, NotificationMessage nm)
+        public async Task SendNotificationToUserAsync(string userId, NotificationMessage nm,string? groupName)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(groupName))
+            {
+                await hubContext.Clients.Group(groupName).SendAsync("NewMessage", nm.Title, nm.Description);
+            }
+            else
+            {
+                await hubContext.Clients.User(userId).SendAsync("NewMessage", nm.Title, nm.Description);
+            }
+            await rp.SendNotification(nm);
+
         }
     }
 }

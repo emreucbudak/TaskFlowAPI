f1767cbbb4800202b29a5b813e39af16e0ab5fe1
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-01-27 17:02:10 +0300

    IDENTİTY MODÜLÜNÜN CQRSİ BİTİRİLDİ

---
+++ src/Modules/Identity/Identity.Application/Features/CQRS/Company/Command/Update/UpdateCompanyCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using FlashMediator;
+
+namespace Identity.Application.Features.CQRS.Company.Command.Update
+{
+    public record UpdateCompanyCommandRequest : IRequest
+    {
+        public Guid Id { get; init; }
+        public string CompanyName { get; init; }
+        public UpdateCompanyCommandRequest(Guid id, string companyName)
+        {
+            Id = id;
+            CompanyName = companyName;
+        }
+    }
+}

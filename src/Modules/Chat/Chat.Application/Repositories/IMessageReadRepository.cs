29bdec5405bba3058643b4e4343db2d30a186c69
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-01-29 21:52:42 +0300

    CHATLEŞME İÇİN REPOSİTORYLER EKLENİYOR

--- src/Modules/Chat/Chat.Application/Repositories/IMessageReadRepository.cs
+++ src/Modules/Chat/Chat.Application/Repositories/IMessageReadRepository.cs
@@ -1,6 +1,51 @@
-﻿namespace Chat.Application.Repositories
+﻿using Chat.Domain.Entities;
+
+namespace Chat.Application.Repositories
 {
-    internal interface IMessageReadRepository
+    public interface IMessageReadRepository
     {
+ 
+        Task<Message> GetByIdAsync(bool trackChanges,Guid id);
+        Task<IEnumerable<Message>> GetMessagesByUserIdAsync(Guid userId);
+
+        Task<IEnumerable<Message>> GetMessagesBetweenUsersAsync(string userId1, string userId2, CancellationToken cancellationToken = default);
+
+
+        Task<IEnumerable<Message>> GetMessagesByGroupIdAsync(string groupId);
+
+   
+        Task<IEnumerable<Message>> GetMessagesPagedAsync(int skip, int take);
+
+       
+        Task<IEnumerable<Message>> GetMessagesSinceDateAsync(DateTime date);
+
+        Task<IEnumerable<Message>> GetMessagesByDateRangeAsync(DateTime startDate, DateTime endDate);
+
+
+        Task<IEnumerable<Message>> GetUnreadMessagesByUserIdAsync(Guid userId);
+
+        Task<int> GetUnreadMessageCountAsync(string userId);
+
+  
+        Task<IEnumerable<Message>> GetLastMessagesAsync(int count);
+
+        Task<IEnumerable<Message>> GetLastGroupMessagesAsync(string groupId, int count);
+
+
+        Task<IEnumerable<Message>> GetLastMessagesBetweenUsersAsync(string userId1, string userId2, int count);
+
+
+        Task<IEnumerable<Message>> SearchMessagesAsync(string searchTerm);
+
+       
+        Task<IEnumerable<Message>> SearchMessagesByUserAsync(string userId, string searchTerm);
+
+
+        Task<bool> ExistsAsync(string id);
+
+
+
+
+
     }
 }

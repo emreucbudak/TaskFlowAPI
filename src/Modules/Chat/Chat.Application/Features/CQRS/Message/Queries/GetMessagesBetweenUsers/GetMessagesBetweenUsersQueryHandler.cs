8f83a5f00300a62149c5cee09e7a260d7c4a84eb
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-08 17:40:01 +0300

    MESAJLARA REDİS EKLENDİ

--- src/Modules/Chat/Chat.Application/Features/CQRS/Message/Queries/GetMessagesBetweenUsers/GetMessagesBetweenUsersQueryHandler.cs
+++ src/Modules/Chat/Chat.Application/Features/CQRS/Message/Queries/GetMessagesBetweenUsers/GetMessagesBetweenUsersQueryHandler.cs
@@ -1,19 +1,31 @@
 using Chat.Application.Repositories;
 using FlashMediator;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace Chat.Application.Features.CQRS.Message.Queries.GetMessagesBetweenUsers
 {
     public class GetMessagesBetweenUsersQueryHandler : IRequestHandler<GetMessagesBetweenUsersQueryRequest, List<GetMessagesBetweenUsersQueryResponse>>
     {
         private readonly IMessageReadRepository _messageReadRepository;
+        private readonly IDistributedCache _distributedCache;
 
-        public GetMessagesBetweenUsersQueryHandler(IMessageReadRepository messageReadRepository)
+        public GetMessagesBetweenUsersQueryHandler(IMessageReadRepository messageReadRepository, IDistributedCache distributedCache)
         {
             _messageReadRepository = messageReadRepository;
+            _distributedCache = distributedCache;
         }
 
         public async Task<List<GetMessagesBetweenUsersQueryResponse>> Handle(GetMessagesBetweenUsersQueryRequest request, CancellationToken cancellationToken)
         {
+            string cacheKey = $"messages_{request.CurrentUserId}_{request.UserId1}_{request.UserId2}_{request.PageSize}_{request.Page}";
+            var cachedMessages = await _distributedCache.GetStringAsync(cacheKey, cancellationToken);
+
+            if (!string.IsNullOrEmpty(cachedMessages))
+            {
+                return JsonSerializer.Deserialize<List<GetMessagesBetweenUsersQueryResponse>>(cachedMessages)!;
+            }
+
             var messages = await _messageReadRepository.GetMessagesBetweenUsersAsync(
                 request.CurrentUserId,
                 request.UserId1,
@@ -22,7 +34,7 @@ namespace Chat.Application.Features.CQRS.Message.Queries.GetMessagesBetweenUsers
                 request.Page
             );
 
-            return messages.Select(m => new GetMessagesBetweenUsersQueryResponse
+            var response = messages.Select(m => new GetMessagesBetweenUsersQueryResponse
             {
                 Id = m.Id,
                 Content = m.Content,
@@ -36,6 +48,14 @@ namespace Chat.Application.Features.CQRS.Message.Queries.GetMessagesBetweenUsers
                 isDelivered = m.isDelivered,
                 DeliveredTime = m.DeliveredTime
             }).ToList();
+
+            var serializedResponse = JsonSerializer.Serialize(response);
+            await _distributedCache.SetStringAsync(cacheKey, serializedResponse, new DistributedCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromMinutes(60)
+            }, cancellationToken);
+
+            return response;
         }
     }
 }

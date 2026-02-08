0681e0d0e5d156a96a4736b45fcf2e4da36ae5ce
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-09 02:37:35 +0300

    GÜVENLİK EKLEMELERİ YAPILIYOR İYİLEŞTİRME YAPILACAK

---
+++ src/Modules/Chat/Chat.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,29 @@
+using Chat.Application.Services;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Chat.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid? UserId
+        {
+            get
+            {
+                var userIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (string.IsNullOrEmpty(userIdClaim) || !Guid.TryParse(userIdClaim, out var userId))
+                {
+                    return null;
+                }
+                return userId;
+            }
+        }
+    }
+}

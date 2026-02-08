819e83cc09fb6d1e6049deae149cd7e6311b06c8
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-08 17:53:09 +0300

    MESAJ EKLEME VE SİGNALR BAĞLANTISI SAĞLANIYOR

---
+++ src/Modules/Chat/Chat.Application/ChatNotification/IChatNotificationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Chat.Application.ChatNotification
+{
+    public interface IChatNotificationService
+    {
+        Task SendMessageToGroupAsync(Guid groupId, object message);
+        Task SendMessageToUserAsync(Guid userId, object message);
+
+    }
+}
---
+++ src/Modules/Chat/Chat.Infrastructure/ChatNotification/ChatNotificationService.cs
@@ -0,0 +1,17 @@
+﻿using Chat.Application.ChatNotification;
+
+namespace Chat.Infrastructure.ChatNotification
+{
+    public class ChatNotificationService : IChatNotificationService
+    {
+        public Task SendMessageToGroupAsync(Guid groupId, object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMessageToUserAsync(Guid userId, object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

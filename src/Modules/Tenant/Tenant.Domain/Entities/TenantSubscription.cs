676d085cd584a446fc4ef01bde3e2d9615e1154a
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-14 16:35:06 +0300

    abonelik sistemi ekleniyor

---
+++ src/Modules/Tenant/Tenant.Domain/Entities/TenantSubscription.cs
@@ -0,0 +1,20 @@
+﻿using TaskFlow.BuildingBlocks.Common;
+
+namespace Tenant.Domain.Entities
+{
+    public class TenantSubscription : BaseEntity
+    {
+        public Guid TenantId { get; private set; }
+
+        public int CompanyPlanId { get; private set; }
+        public  CompanyPlan CompanyPlan { get; private set; }
+
+        public string PaymentProviderSubscriptionId { get; private set; }
+
+        public SubscriptionStatus Status { get; private set; }
+
+        public DateTime StartDate { get; private set; }
+        public DateTime NextBillingDate { get; private set; }
+        public DateTime? CanceledAt { get; private set; }
+    }
+}

90dc53de02f52c158e8c299ed8d8495ea31b5621
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-18 17:23:54 +0300

    DEADLİNE NOTİFİCATİON EKLENDİ

--- src/Modules/ProjectManagement/ProjectManagement.Application/Features/CQRS/Tasks/Command/Create/CreateTasksCommandHandler.cs
+++ src/Modules/ProjectManagement/ProjectManagement.Application/Features/CQRS/Tasks/Command/Create/CreateTasksCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using FlashMediator;
+using DotNetCore.CAP;
+using FlashMediator;
+using ProjectManagement.Application.IntegrationEvents;
 using ProjectManagement.Application.Repositories;
 using TaskFlow.BuildingBlocks.UnitOfWork;
 
@@ -7,19 +9,59 @@ namespace ProjectManagement.Application.Features.CQRS.Tasks.Command.Create
     public class CreateTasksCommandHandler : IRequestHandler<CreateTasksCommandRequest>
     {
         private readonly IProjectManagementWriteRepository _writeRepository;
-        private readonly IUnitOfWork unitOfWork;
+        private readonly ICapUnitOfWork _unitOfWork;
+        private readonly ICapPublisher _capPublisher;
 
-        public CreateTasksCommandHandler(IUnitOfWork unitOfWork, IProjectManagementWriteRepository writeRepository)
+        public CreateTasksCommandHandler(
+            ICapUnitOfWork unitOfWork,
+            IProjectManagementWriteRepository writeRepository,
+            ICapPublisher capPublisher)
         {
-            this.unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;
             _writeRepository = writeRepository;
+            _capPublisher = capPublisher;
         }
 
         public async Task Handle(CreateTasksCommandRequest request, CancellationToken cancellationToken)
         {
-            var task = new Domain.Entities.Task(request.TaskName, request.Description, DateOnly.FromDateTime(request.DeadlineTime),DateOnly.FromDateTime(DateTime.UtcNow));
-            await _writeRepository.AddTask(task);
-            await unitOfWork.SaveChangesAsync(cancellationToken);
+            var deadlineDate = DateOnly.FromDateTime(request.DeadlineTime);
+            var task = new Domain.Entities.Task(
+                request.TaskName,
+                request.Description,
+                deadlineDate,
+                DateOnly.FromDateTime(DateTime.UtcNow));
+            var delayTime = deadlineDate
+                .ToDateTime(TimeOnly.MinValue, DateTimeKind.Utc)
+                .AddDays(-1) - DateTime.UtcNow;
+
+            if (delayTime < TimeSpan.Zero)
+            {
+                delayTime = TimeSpan.Zero;
+            }
+
+            await using var transaction = _unitOfWork.BeginTransaction(_capPublisher, autoCommit: false);
+            try
+            {
+                await _writeRepository.AddTask(task);
+
+                await _capPublisher.PublishDelayAsync(
+                    delayTime,
+                    "TaskCreated",
+                    new TaskCreatedIntegrationEvent(
+                        task.Id,
+                        task.TaskName,
+                        task.Description,
+                        task.DeadlineTime),
+                    cancellationToken: cancellationToken);
+
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+                await transaction.CommitAsync(cancellationToken);
+            }
+            catch
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                throw;
+            }
         }
     }
 }

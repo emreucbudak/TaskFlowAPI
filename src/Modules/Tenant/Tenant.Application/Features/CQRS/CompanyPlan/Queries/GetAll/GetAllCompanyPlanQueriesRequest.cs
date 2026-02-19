1c6efca9d7dbeb1bab7e06ce3d50d63abbfdfe27
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-02-19 20:52:19 +0300

    WEB İÇİN GEREKEN GEREKLİLİKLER EKLENDİ

--- src/Modules/Tenant/Tenant.Application/Features/CQRS/CompanyPlan/Queries/GetAll/GetAllCompanyPlanQueriesRequest.cs
+++ src/Modules/Tenant/Tenant.Application/Features/CQRS/CompanyPlan/Queries/GetAll/GetAllCompanyPlanQueriesRequest.cs
@@ -1,12 +1,8 @@
 ﻿using FlashMediator;
-using TaskFlow.BuildingBlocks.Interfaces;
 
 namespace Tenant.Application.Features.CQRS.CompanyPlan.Queries.GetAll
 {
-    public class GetAllCompanyPlanQueriesRequest : IRequest<List<GetAllCompanyPlanQueriesResponse>>, ICacheableQuery
+    public class GetAllCompanyPlanQueriesRequest : IRequest<List<GetAllCompanyPlanQueriesResponse>>
     {
-        public string CacheKey => "getallcompanyplan";
-
-        public TimeSpan? ExpirationTime => TimeSpan.FromMinutes(30);
     }
 }
